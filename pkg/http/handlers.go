6980d1cbdde133774522052c64e6f6cc969dbb0e
Matt1-Mac <matthewmrph40@gmail.com>
2022-02-06 22:49:18 -0500

    edit routes

--- pkg/http/handlers.go
+++ pkg/http/handlers.go
@@ -1,91 +1,92 @@
 package http
 
 import (
-  "encoding/json"
-  "io/ioutil"
-  "net/http"
-  "strconv"
-  "github.com/julienschmidt/httprouter"
-  "github.com/{{ YOUR_GITHUB_USERNAME }}/kudo-oos/pkg/core"
-  "github.com/{{ YOUR_GITHUB_USERNAME }}/kudo-oos/pkg/kudo"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"strconv"
+
+	"github.com/julienschmidt/httprouter"
+	"github.com/matthew-murphy/kudo-oos/pkg/core"
+	"github.com/matthew-murphy/kudo-oos/pkg/kudo"
 )
 
 type Service struct {
-  repo   core.Repository
-  Router http.Handler
+	repo   core.Repository
+	Router http.Handler
 }
 
 func New(repo core.Repository) Service {
-  service := Service{
-    repo: repo,
-  }
+	service := Service{
+		repo: repo,
+	}
 
-  router := httprouter.New()
-  router.GET("/kudos", service.Index)
-  router.POST("/kudos", service.Create)
-  router.DELETE("/kudos/:id", service.Delete)
-  router.PUT("/kudos/:id", service.Update)
+	router := httprouter.New()
+	router.GET("/kudos", service.Index)
+	router.POST("/kudos", service.Create)
+	router.DELETE("/kudos/:id", service.Delete)
+	router.PUT("/kudos/:id", service.Update)
 
-  service.Router = UseMiddlewares(router)
+	service.Router = UseMiddlewares(router)
 
-  return service
+	return service
 }
 
 func (s Service) Index(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
-  service := kudo.NewService(s.repo, r.Context().Value("userId").(string))
-  kudos, err := service.GetKudos()
+	service := kudo.NewService(s.repo, r.Context().Value("userId").(string))
+	kudos, err := service.GetKudos()
 
-  if err != nil {
-    w.WriteHeader(http.StatusInternalServerError)
-    return
-  }
-  w.WriteHeader(http.StatusOK)
-  json.NewEncoder(w).Encode(kudos)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(kudos)
 }
 
 func (s Service) Create(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
-  service := kudo.NewService(s.repo, r.Context().Value("userId").(string))
-  payload, _ := ioutil.ReadAll(r.Body)
+	service := kudo.NewService(s.repo, r.Context().Value("userId").(string))
+	payload, _ := ioutil.ReadAll(r.Body)
 
-  githubRepo := kudo.GitHubRepo{}
-  json.Unmarshal(payload, &githubRepo)
+	githubRepo := kudo.GitHubRepo{}
+	json.Unmarshal(payload, &githubRepo)
 
-  kudo, err := service.CreateKudoFor(githubRepo)
+	kudo, err := service.CreateKudoFor(githubRepo)
 
-  if err != nil {
-    w.WriteHeader(http.StatusInternalServerError)
-    return
-  }
-  w.WriteHeader(http.StatusCreated)
-  json.NewEncoder(w).Encode(kudo)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(kudo)
 }
 
 func (s Service) Delete(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
-  service := kudo.NewService(s.repo, r.Context().Value("userId").(string))
+	service := kudo.NewService(s.repo, r.Context().Value("userId").(string))
 
-  repoID, _ := strconv.Atoi(params.ByName("id"))
-  githubRepo := kudo.GitHubRepo{RepoID: int64(repoID)}
+	repoID, _ := strconv.Atoi(params.ByName("id"))
+	githubRepo := kudo.GitHubRepo{RepoID: int64(repoID)}
 
-  _, err := service.RemoveKudo(githubRepo)
-  if err != nil {
-    w.WriteHeader(http.StatusInternalServerError)
-    return
-  }
-  w.WriteHeader(http.StatusOK)
+	_, err := service.RemoveKudo(githubRepo)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
 }
 
 func (s Service) Update(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
-  service := kudo.NewService(s.repo, r.Context().Value("userId").(string))
-  payload, _ := ioutil.ReadAll(r.Body)
+	service := kudo.NewService(s.repo, r.Context().Value("userId").(string))
+	payload, _ := ioutil.ReadAll(r.Body)
 
-  githubRepo := kudo.GitHubRepo{}
-  json.Unmarshal(payload, &githubRepo)
+	githubRepo := kudo.GitHubRepo{}
+	json.Unmarshal(payload, &githubRepo)
 
-  kudo, err := service.UpdateKudoWith(githubRepo)
-  if err != nil {
-    w.WriteHeader(http.StatusInternalServerError)
-    return
-  }
-  w.WriteHeader(http.StatusOK)
-  json.NewEncoder(w).Encode(kudo)
+	kudo, err := service.UpdateKudoWith(githubRepo)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(kudo)
 }
